335dc8c472c969fde9fdd1fe7856ab2f42e03bf0
DipikaRane <deepikajoge@gmail.com>
2022-07-14 17:17:25 +0530

    BTS4, BTS5, Javascript

---
+++ Javascript/DataTypes.js
@@ -0,0 +1,48 @@
+//JS supports three datatypes
+//string, number, boolean
+
+//string --anything that comes in  " ",' ',``,
+"Hii", 'Hiii', `HIII`, "2321", "hjdsl873283" ,"true", "false"
+
+//number -- any number integer, decimal, whole number
+672, 678326832329, 6832.8909
+
+//boolean -- true and false
+true=1
+false=0
+
+//es5
+//var -- it has global scope, can be redeclared and reassigned
+a=10
+var a=10
+var b="Techvanto"
+var c=true
+
+//es6
+//let -- improved version of var, scope:block, can not redeclare but can be reassigned
+let a=10;
+undefined
+a
+10
+let a=11;
+undefined
+a
+11
+
+//const -- functional scope, can not be redeclared and can not be reassigned
+const a=10;
+//answer -- Uncaught SyntaxError: Identifier 'a' has already been declared
+const f="HII"
+"HII"
+//inbuilt method
+var a=10;
+typeof(a)
+'number'
+var n="HIII"
+undefined
+typeof(n)
+'string'
+var h=false
+undefined
+typeof(h)
+'boolean'
