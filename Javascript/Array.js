fa764c17443d9f63f174e54745597a38be69fae4
DipikaRane <deepikajoge@gmail.com>
2022-07-19 12:21:31 +0530

    Array

---
+++ Javascript/Array.js
@@ -0,0 +1,112 @@
+//Array -- collection of homogeneous data/dataypes
+
+var a=['a','b','c','d','e','f','g','h','i'] // array of string
+var b=[1,2,3,4,5,6,7]       // array of numbers
+var c=[true,false,false,true,true]      // array of booleans
+
+//in JavaScript
+var d=['a','b','c',1,2,3,4,5,true,false,true] // array of string,numbers and booleans
+
+var city=["Delhi","Mumbai","Chennai","Nagpur","Kolkata","Chandigarh"]
+city.length
+6
+typeof(city)
+'object'
+city[0]
+'Delhi'
+
+city[3]
+'Nagpur'
+
+city[city.length-1]
+'Chandigarh'
+
+var city=["Delhi","Mumbai","Chennai","Nagpur","Kolkata","Chandigarh"]
+//push() - to add the object/data
+city.push("Ranchi")
+7
+city
+(7) ['Delhi', 'Mumbai', 'Chennai', 'Nagpur', 'Kolkata', 'Chandigarh', 'Ranchi']
+city.unshift("Hyderabad")
+8
+city
+(8) ['Hyderabad', 'Delhi', 'Mumbai', 'Chennai', 'Nagpur', 'Kolkata', 'Chandigarh', 'Ranchi']
+city.pop()
+'Ranchi'
+city.pop(1)
+'Chandigarh'
+city
+(6) ['Hyderabad', 'Delhi', 'Mumbai', 'Chennai', 'Nagpur', 'Kolkata']
+city.pop(3)
+'Kolkata'
+city
+(5) ['Hyderabad', 'Delhi', 'Mumbai', 'Chennai', 'Nagpur']
+
+push() //-- add object at end of array
+pop()  //-- removes object from end of array
+unshift() //add object at start of array
+shift() //--removes object from start
+
+//slice and splice on Array
+var city=["Delhi","Mumbai","Chennai","Nagpur","Kolkata","Chandigarh"]
+city.slice(1)
+(5) ['Mumbai', 'Chennai', 'Nagpur', 'Kolkata', 'Chandigarh']
+var city=["Delhi","Mumbai","Chennai","Nagpur","Kolkata","Chandigarh"]
+city.slice(2,5)
+
+arrayName.splice(start,deleteCount,array)
+(3) ['Chennai', 'Nagpur', 'Kolkata']
+city.splice(3,0,"Banglore")
+
+city
+(7) ['Delhi', 'Mumbai', 'Chennai', 'Banglore', 'Nagpur', 'Kolkata', 'Chandigarh']
+city.splice(3,0,"Tirupati","Ooty")
+
+city
+(9) ['Delhi', 'Mumbai', 'Chennai', 'Tirupati', 'Ooty', 'Banglore', 'Nagpur', 'Kolkata', 'Chandigarh']
+city.splice(6,0,"Gondiya","Wardha")
+
+city
+(11) ['Delhi', 'Mumbai', 'Chennai', 'Tirupati', 'Ooty', 'Banglore', 'Gondiya', 'Wardha', 'Nagpur', 'Kolkata', 'Chandigarh']
+
+city.splice(1,1)
+[0]
+city
+(10) ['Delhi', 'Chennai', 'Tirupati', 'Ooty', 'Banglore', 'Gondiya', 'Wardha', 'Nagpur', 'Kolkata', 'Chandigarh']
+city.splice(1,1,"Mumbai")
+['Chennai']
+city
+(10) ['Delhi', 'Mumbai', 'Tirupati', 'Ooty', 'Banglore', 'Gondiya', 'Wardha', 'Nagpur', 'Kolkata', 'Chandigarh']
+
+//use of concat in array
+var a=[1,2,3,4,5]
+var b=[1,'c','d',6]
+a+b                     //this will prove wrong
+'1,2,3,4,51,c,d,6'
+a.concat(b)
+(9) [1, 2, 3, 4, 5, 1, 'c', 'd', 6]
+
+//sort()
+var city=["Delhi","Mumbai","Chennai","Nagpur","Kolkata","Chandigarh"]
+city.sort()
+(6) ['Chandigarh', 'Chennai', 'Delhi', 'Kolkata', 'Mumbai', 'Nagpur']
+Array.isArray(city)
+true
+var a=64891
+undefined
+Array.isArray(a)
+false
+
+//Multidimenssional Array
+var studentData=[['John',23],["Kiv",22],["Donald",21]]
+
+let student1=['John',23]
+let student2=["Kiv",22]
+let student3=["Donald",21]
+
+var studentData=[student1,student2,student3]
+studentData
+(3) [Array(2), Array(2), Array(2)]
+0:(2) ['John', 23]
+1: (2) ['Kiv', 22]
+2: (2) ['Donald', 21]
--- Javascript/StringOperations.js
+++ Javascript/StringOperations.js
@@ -33,3 +33,116 @@ name.trim()
 name.trim().length
 12
 
+// JS is case insensitive
+var name1="techvanto"
+var name2="Techvanto"
+name1==name2
+false
+name1.toLowerCase()==name2.toLowerCase()
+tru
+
+//charAt()
+var city="amrITsar"
+city.charAt(0)
+'a'
+city.charAt(5)
+'s'
+city.charAt(3)
+'I'
+city.charAt(3).toLowerCase()
+'i'
+city.charAt(4).toLowerCase()
+'t'
+
+//slice() --- it slices/ cuts the particular character from the string
+var city="amrITsar"
+city.slice(1)
+'mrITsar'
+city.slice(1,4)
+'mrI'
+city.slice(-1)
+'r'
+city.slice(-4)
+'Tsar'
+
+//to convert string in format by using charAt, slice and toLowerCase() and toUpperCase()
+city.charAt(0).toUpperCase()
+'A'
+city.slice(1)
+'mrITsar'
+city.slice(1).toLowerCase()
+'mritsar'
+city.charAt(0).toUpperCase() + city.slice(1).toLowerCase() //concat
+'Amritsar'
+
+//replace() -- it replaces the string , word, characters etc.
+ var a="I am learning in Techvanto"
+ a.replace(/learning/,'teaching')
+ 'I am teaching in Techvanto'
+
+ var a="Techvanto I am learning in Techvanto"
+ a.replace(/Techvanto/,'LPU')
+'LPU I am learning in Techvanto'
+
+ var a="Techvanto I am Techvanto learning in Techvanto"
+ a.replace(/Techvanto/,'LPU')
+'LPU I am Techvanto learning in Techvanto'
+
+var a="Techvanto I am Techvanto learning in Techvanto"
+a.replace(/Techvanto/g,'LPU')       // g means global that replace every similar word
+'LPU I am LPU learning in LPU'
+
+var name='   TECHVANTO  !  '
+name.replace(/ /g,'')
+
+//split()
+var url="G:/Techvanto/Batch2/CSS/Listing.html"
+var out=url.split('/')
+out
+(5) ['G:', 'Techvanto', 'Batch2', 'CSS', 'Listing.html']
+var url="G:/Techvanto/Batch2/CSS/Listing.html"
+var out=url.split('.')
+out
+(2) ['G:/Techvanto/Batch2/CSS/Listing', 'html']
+
+var url="G:/Techvanto/Batch2/CSS/Listing.html"
+var out=url.split('/')
+out
+['G:/Techvanto/Batch2/CSS/Listing', 'html']
+out[out.length-1]
+'html'
+out[out.length-2]
+'G:/Techvanto/Batch2/CSS/Listing'
+
+var a='Javascript is a language'
+undefined
+a.split(' ')
+(4) ['Javascript', 'is', 'a', 'language']
+a.split(' ')[0]
+'Javascript'
+var name=a.split(' ')[0]
+undefined
+name
+'Javascript'
+var out=name.split('')
+undefined
+out
+(10) ['J', 'a', 'v', 'a', 's', 'c', 'r', 'i', 'p', 't']
+
+//use of toString()
+out.toString()
+'J,a,v,a,s,c,r,i,p,t'
+out.toString().replace(/,/g,'')
+'Javascript'
+
+var a=09325098
+a.toString()
+'9325098'
+
+var a=90325098
+a.toString()
+'90325098'
+a.toString().slice(0,-1)
+'9032509'
+a.toString().slice(0,-4)
+'9032'
